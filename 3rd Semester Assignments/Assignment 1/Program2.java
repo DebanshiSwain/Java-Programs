12b5c1748ae81c477154e0aee3168e024f9f2f12
DebanshiSwain <debanshiswain11@gmail.com>
2025-09-07 21:16:32 +0530

    Program2.java

---
+++ 3rd Semester Assignments/Assignment 1/Program2.java
@@ -0,0 +1,11 @@
+// 2. Write a program to display the addition result of two  numbers 10.25 and 20.55 on the screen
+
+class P2
+{
+    public static void main(String[] args)
+    {
+        double x = 10.25, y = 20.55;
+        double sum = x + y;
+        System.out.println("Sum=" + sum);
+    }
+}
