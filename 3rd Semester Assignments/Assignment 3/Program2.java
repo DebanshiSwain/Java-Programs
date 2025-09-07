2b83ed56f4bd4f970a5ed26b6ac955cd297dd6ea
DebanshiSwain <debanshiswain11@gmail.com>
2025-09-07 21:53:03 +0530

    Program2.java

---
+++ 3rd Semester Assignments/Assignment 3/Program2.java
@@ -0,0 +1,31 @@
+// 2. Write a program to sort the array of n elements.
+
+import java.util.Scanner;
+class P2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
