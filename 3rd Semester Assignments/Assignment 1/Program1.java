28cb7ff043fa5f0b16c34219b5569ad06895c7fc
DebanshiSwain <debanshiswain11@gmail.com>
2025-09-07 21:14:08 +0530

    Added Program1.java for Assignment 1

---
+++ 3rd Semester Assignments/Assignment 1/Program1.java
@@ -0,0 +1,21 @@
+/* 1.Write a program to display your name, branch, roll no, and college name on the computer screen.*/
+import java.util.Scanner;
+class P1
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(system.in);
+    System.out.println(“Enter your name”);
+    String name=sc.nextLine();
+    System.out.println(“Enter your branch”);
+    String branch=sc.nextLine();
+    System.out.println(“Enter your roll no”);
+    int roll=sc.nextInt();
+    System.out.println(“Enter your college”);
+    String college=sc.nextLine();
+    System.out.println(“Name:”+name);
+    System.out.println(“Roll:”+roll);
+    System.out.println(“Branch:”+branch);
+    System.out.println(“College:"+college);
+  }
+}
