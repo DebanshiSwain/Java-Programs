97f0e0081b4a789f61be6f494b95bf906c724b91
DebanshiSwain <debanshiswain11@gmail.com>
2025-09-07 21:24:10 +0530

    Program8.java

---
+++ 3rd Semester Assignments/Assignment 1/Program8.java
@@ -0,0 +1,21 @@
+// 8. Write a program to reverse a number.
+
+import java.util.Scanner;
+class P8
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int rev = 0;
+        
+        while (n != 0)
+        {
+            rev = rev * 10 + (n % 10);
+            n = n / 10;
+        }
+        
+        System.out.println("Reversed Number: " + rev);
+    }
+}
