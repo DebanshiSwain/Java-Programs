649062c6a5f396ea902abb1a70a02f934795de5c
DebanshiSwain <debanshiswain11@gmail.com>
2025-09-07 21:49:08 +0530

    Program12.java

---
+++ 3rd Semester Assignments/Assignment 2/Program12.java
@@ -0,0 +1,19 @@
+// 12. Write a recursive program to find the sum of n natural numbers. [n is user input]
+
+import java.util.Scanner;
+class P12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        
+        System.out.println("Sum of first " + n + " natural numbers is " + sum(n));
+    }
+    
+    static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
+}
